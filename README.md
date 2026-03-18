d664ac8de4d1ee6056ff27455c8da47c3ea29668
Michelle Angeline <michelle11angeline@gmail.com>
2026-03-18 12:07:22 +0800

    Modified the main program

---
+++ README.md
@@ -0,0 +1,14 @@
+#Grid Navigation Game
+
+A project for Unix and C programming unit 
+This is Assignment one where the task is to design, code and test a Grid navigation game based on C language and 2D grid system I have learnt. 
+
+## Files
+- main.c
+- game.c
+- grid.c
+- move.c
+- game.h
+- grid.h
+- move.h
+- Makefile
--- main.c
+++ main.c
@@ -1,18 +1,29 @@
+/*main.c - recieve user's input and start the game*/
 #include <stdio.h>
 #include <stdlib.h>
-#include "grid.h"
+#include "game.h" 
 
-int main(void) {
-    int size = 5;
+/*main function to generate user's command line input*/
+int main(int argc, char *argv[]) { 
+    if (argc != 6) { /*Check the input, if false print usage prompt and terminate*/
+        printf("Usage: %s <grid_size> <key_cell> <trap1> <trap2> <trap3>\n", argv[0]);
+        return 1;
+    }
+    
+    /*Parsing single number from user's command line input */
+    int grid_size = atoi(argv[1]); 
+    int key_cell = atoi(argv[2]); 
+    int trap1 = atoi(argv[3]); 
+    int trap2 = atoi(argv[4]);
+    int trap3 = atoi(argv[5]);
 
-    char **grid = createGrid(size);
-    if (grid == NULL) {
-        printf("Memory allocation failed\n");
+    /*Check the input for the gridsize*/
+    if (grid_size < 3 || grid_size > 6) { 
+        printf("Grid size must be between 3 and 6.\n");
         return 1;
     }
 
-    printGrid(grid, size);
-
-    freeGrid(grid, size);
+    /*initialize the game*/
+    startGame(grid_size, key_cell, trap1, trap2, trap3);
     return 0;
 }
