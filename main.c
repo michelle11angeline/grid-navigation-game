6c990d5e3a2a9745f9ac373ed2d6626a2ea63a98
Michelle Angeline <michelle11angeline@gmail.com>
2026-03-20 22:43:37 +0800

    Update CLI to accept grid size and output filename

--- main.c
+++ main.c
@@ -1,29 +1,27 @@
-/*main.c - recieve user's input and start the game*/
 #include <stdio.h>
 #include <stdlib.h>
-#include "game.h" 
+#include <time.h>
+#include "game.h"
 
-/*main function to generate user's command line input*/
 int main(int argc, char *argv[]) { 
-    if (argc != 6) { /*Check the input, if false print usage prompt and terminate*/
-        printf("Usage: %s <grid_size> <key_cell> <trap1> <trap2> <trap3>\n", argv[0]);
+    int grid_size;
+    const char *output_file;
+
+    if (argc != 3) {
+        printf("Usage: %s <grid_size> <file name>\n", argv[0]);
         return 1;
     }
     
-    /*Parsing single number from user's command line input */
-    int grid_size = atoi(argv[1]); 
-    int key_cell = atoi(argv[2]); 
-    int trap1 = atoi(argv[3]); 
-    int trap2 = atoi(argv[4]);
-    int trap3 = atoi(argv[5]);
+    grid_size = atoi(argv[1]);
+    output_file = argv[2];
 
-    /*Check the input for the gridsize*/
-    if (grid_size < 3 || grid_size > 6) { 
-        printf("Grid size must be between 3 and 6.\n");
+    if (grid_size < MIN_GRID_SIZE || grid_size > MAX_GRID_SIZE) {
+        printf("Grid size must be between %d and %d.\n", MIN_GRID_SIZE, MAX_GRID_SIZE);
         return 1;
     }
 
-    /*initialize the game*/
-    startGame(grid_size, key_cell, trap1, trap2, trap3);
+    srand(time(NULL));
+
+    startGame(grid_size, output_file);
     return 0;
 }
